bdc30bd0d3f4d2d3565b3d18bdf763c928f08f23
tariktesfa <chere.lemma@aastu.edu.et>
2023-04-28 12:35:37 +0300

    Install react-router-dom package

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import NavBar from './components/Navbar';
+import Home from './components/Cryptolist';
+import CryptoDetail from './components/Cryptodetails';
 
 function App() {
   return (
-    <div />
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="home" element={<Home />} />
+        <Route path="coinDetail/:id" element={<CryptoDetail />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
