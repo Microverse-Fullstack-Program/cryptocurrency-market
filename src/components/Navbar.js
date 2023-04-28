e663090eb38daa8e43266e35a9410985b06a737e
tariktesfa <chere.lemma@aastu.edu.et>
2023-04-28 12:02:28 +0300

    Create Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import { Link, useLocation } from 'react-router-dom';
+import logo from '../assets/crypto-logo.jpeg';
+
+const NavBar = () => {
+  const location = useLocation();
+
+  return (
+    <header className="header">
+      <div className="header-left">
+        {location.pathname !== '/' ? (
+          <span>
+            <Link to="/">
+              <i className="ri-arrow-left-s-line" />
+            </Link>
+          </span>
+        ) : <img className="logo" src={logo} alt="crypto logo" />}
+        <span>YayCriptoMarket</span>
+      </div>
+      <div className="header-center">
+        <h5 data-testid="title">Crypto Market</h5>
+      </div>
+      <div className="header-right">
+        <span>
+          <i className="ri-mic-line" />
+        </span>
+        <span>
+          <i className="ri-settings-5-fill" />
+        </span>
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
