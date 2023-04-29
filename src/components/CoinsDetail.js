cb6bb6b700c68f07f4d0dddc6571f054bf1bc6a3
cherelemma <chere.lemma@aastu.edu.et>
2023-04-29 16:09:32 +0300

    Render coins detail

--- src/components/CoinsDetail.js
+++ src/components/CoinsDetail.js
@@ -1,5 +1,18 @@
-const Cryptodetails = () => {
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
+const Cryptodetails = () => {
+  const { id } = useParams();
+  const { cryptoCurrency } = useSelector((state) => state.crypto);
+  const coinDetail = cryptoCurrency.filter((coin) => coin.id === id);
+
+  return (
+    <div>
+      <h6>{coinDetail[0].name}</h6>
+      <h5>{coinDetail[0].symbol}</h5>
+      <span>{coinDetail[0].current_price}</span>
+    </div>
+  );
 };
 
 export default Cryptodetails;
